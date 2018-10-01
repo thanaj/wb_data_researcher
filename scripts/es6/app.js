1749e411965720d4f2eb64422d48095fc7251cee
thanaj <12088991+thanaj@users.noreply.github.com>
2018-10-01 21:25:31 +0200

    Update app.js

--- scripts/es6/app.js
+++ scripts/es6/app.js
@@ -5,15 +5,3 @@ import {default as getCountries} from './getCountries.js';
 getCountries.init()
 
 
-/*
-sprawdź czy jest baza
-sprawdż czy lista państw jest na bazie
-jeśli jest to ją pobierz
-jeśli nie ma to pobierz z netu
-i zapisz ja na bazie
-
-
-
-
-
-*/
